a67b6c7d00dc8fc2827b082d8185180ef99587b7
陈钦辉 <chenqinhui@didichuxing.com>
2017-09-09 20:40:58 +0800

    @dev udpate

---
+++ demo.js
@@ -0,0 +1,10 @@
+//  https://www.codewars.com/kata/unique-in-order/train/javascript
+const assert = require('assert');
+var fun = function() {
+};
+
+describe('desc', function() {
+  it('1', function() {
+    assert.deepEqual(fun('1'), undefined);
+  });
+});
--- src/fundamentals/6kyu/unique-in-order.js
+++ src/fundamentals/6kyu/unique-in-order.js
@@ -1,5 +1,7 @@
 //  https://www.codewars.com/kata/unique-in-order/train/javascript
 const assert = require('assert');
+
+// my
 var uniqueInOrder = function(iterable) {
   return (Object.prototype.toString.apply(iterable) == '[object Array]'
     ? iterable
@@ -8,6 +10,24 @@ var uniqueInOrder = function(iterable) {
         arr = arguments[2];
     return val !== arr[curIndex - 1];
   });
+};
+
+// clever
+var uniqueInOrder1 = function(iterable) {
+  return [].filter.call(iterable, function(current, index) {
+    return iterable[index - 1] !== current;
+  });
+};
+
+// best
+var uniqueInOrder2 = function(iterable) {
+  var i, last, result = [];
+  for (i = 0; i < iterable.length; i++) {
+    if (iterable[i] !== last) {
+      result.push(last = iterable[i]);
+    }
+  }
+  return result;
 };
 
 describe('uniqueInOrder', function() {
@@ -19,5 +39,27 @@ describe('uniqueInOrder', function() {
   });
   it('[1,2,2,3,3]', function() {
     assert.deepEqual(uniqueInOrder([1, 2, 2, 3, 3]), [1, 2, 3]);
+  });
+
+
+  it('AAAABBBCCDAABBB', function() {
+    assert.deepEqual(uniqueInOrder1('AAAABBBCCDAABBB'), ['A', 'B', 'C', 'D', 'A', 'B']);
+  });
+  it('ABBCcAD', function() {
+    assert.deepEqual(uniqueInOrder1('ABBCcAD'), ['A', 'B', 'C', 'c', 'A', 'D']);
+  });
+  it('[1,2,2,3,3]', function() {
+    assert.deepEqual(uniqueInOrder1([1, 2, 2, 3, 3]), [1, 2, 3]);
+  });
+
+
+  it('AAAABBBCCDAABBB', function() {
+    assert.deepEqual(uniqueInOrder2('AAAABBBCCDAABBB'), ['A', 'B', 'C', 'D', 'A', 'B']);
+  });
+  it('ABBCcAD', function() {
+    assert.deepEqual(uniqueInOrder2('ABBCcAD'), ['A', 'B', 'C', 'c', 'A', 'D']);
+  });
+  it('[1,2,2,3,3]', function() {
+    assert.deepEqual(uniqueInOrder2([1, 2, 2, 3, 3]), [1, 2, 3]);
   });
 });
